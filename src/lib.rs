7b7067bd6aeae1e668c284f01ce973470cb7db55
むずでょ <muzudho1@gmail.com>
2020-11-05 22:23:43 +0900

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,28 @@
 pub mod sequence;
 pub mod tree;
+use std::cell::RefCell;
+use std::rc::Rc;
+
+/// Linked List を作るには工夫が必要です。
+/// [Rustでdoubly linked list](https://blog.ymgyt.io/entry/2019/08/17/013313)
+///
+/// `Rc` - 参照の所有者を複数にします。
+/// `RefCell` - ランタイム時に同時に１つの参照先だけ値が変更できるようにします。
+/// だいたい Box<> の感覚で使う事ができます。
+type Link<T> = Rc<RefCell<T>>;
 
 pub struct SequenceBuilder<T> {
     sequence: Vec<T>,
-    head: Option<Box<SequenceVal<T>>>,
-    tail: Option<Box<SequenceVal<T>>>,
+    head: Option<Link<SequenceVal<T>>>,
+    tail: Option<Link<SequenceVal<T>>>,
 }
 
 #[derive(Clone)]
 pub struct SequenceVal<T> {
     sequence: Vec<T>,
     cursor: usize,
-    head: Option<Box<SequenceVal<T>>>,
-    tail: Option<Box<SequenceVal<T>>>,
+    head: Option<Link<SequenceVal<T>>>,
+    tail: Option<Link<SequenceVal<T>>>,
 }
 
 #[derive(Clone)]
